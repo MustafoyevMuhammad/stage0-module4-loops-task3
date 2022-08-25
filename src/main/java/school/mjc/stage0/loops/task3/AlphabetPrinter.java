6f97f10444a60d487675f2b41814c4c7ca6f5fce
MustafoyevMuhammad <110478566+MustafoyevMuhammad@users.noreply.github.com>
2022-08-25 17:38:51 +0500

    Update AlphabetPrinter.java

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.task3;
 public class AlphabetPrinter {
     public void printAlphabet() {
         for (int i = 'a'; i <= 'z'; i++) {
-            System.out.println((char)a);
+            System.out.println((char)i);
         }
     }
 }
